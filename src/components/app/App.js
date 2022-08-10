902edf8775ceaa5bc7c7e6c60f0c3788d0899ff4
SyzonVOV <sizonenko89@gmail.com>
2022-08-10 22:13:35 +0300

    feat: 404 lazy loading

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,11 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
+import { MainPage, ComicsPage, SingleComicPage } from '../pages';
+
 import AppHeader from '../appHeader/AppHeader';
+
+const Page404 = lazy(() => import('../pages/404'));
 
 const App = () => {
   return (
@@ -9,12 +13,14 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/comics" element={<ComicsPage />} />
-            <Route path="/comics/:comicId" element={<SingleComicPage />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
+          <Suspense fallback={<h2>Loading...</h2>}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/comics" element={<ComicsPage />} />
+              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </Router>

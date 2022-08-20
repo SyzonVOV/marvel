4a51f21135d817102213679c3becc3561bc9d001
SyzonVOV <sizonenko89@gmail.com>
2022-08-20 13:29:39 +0300

    ref: Refactor the page for single char/comic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,18 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { MainPage, ComicsPage, SingleComicPage } from '../pages';
+import { MainPage, ComicsPage } from '../pages';
 
 import AppHeader from '../appHeader/AppHeader';
 
 const Page404 = lazy(() => import('../pages/404'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
+const SingleComicLayout = lazy(() =>
+  import('../pages/singleComicLayout/SingleComicLayout'),
+);
+const SingleCharacterLayout = lazy(() =>
+  import('../pages/singleCharacterLayout/SingleCharacterLayout'),
+);
 
 const App = () => {
   return (
@@ -17,7 +24,21 @@ const App = () => {
             <Routes>
               <Route path="/" element={<MainPage />} />
               <Route path="/comics" element={<ComicsPage />} />
-              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route
+                path="/comics/:id"
+                element={
+                  <SinglePage Component={SingleComicLayout} dataType="comic" />
+                }
+              />
+              <Route
+                path="/characters/:id"
+                element={
+                  <SinglePage
+                    Component={SingleCharacterLayout}
+                    dataType="character"
+                  />
+                }
+              />
               <Route path="*" element={<Page404 />} />
             </Routes>
           </Suspense>
